cbfb4ef5a6d5c29a382e7b6a4913a053c566a78e
Muhammad Qazafi Sheikh <qazafi.sheikh121@gmail.com>
2023-07-16 12:34:51 +0500

    Delete app/Components_Updated/Components directory

--- app/Components_Updated/Components/Store/actiontype.js
+++
@@ -1,9 +0,0 @@
-const cardetail = 'cardetail'
-const socketConnection = 'socketConnection'
-const newsFeed = 'newsFeed'
-const user = 'user'
-const dylink = 'dylink'
-const addnotification='addnotification'
-const setnotification='setnotification'
-
-export { cardetail, socketConnection, addnotification, setnotification,newsFeed, user, dylink }
