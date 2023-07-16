41c4c6018dbb769f88e07669533b379b3b32152e
Muhammad Qazafi Sheikh <qazafi.sheikh121@gmail.com>
2023-07-16 12:34:24 +0500

    Delete app/Sawari_old/Components directory

--- app/Sawari_old/Components/Store/actiontype.js
+++
@@ -1,7 +0,0 @@
-const cardetail = 'cardetail'
-const socketConnection = 'socketConnection'
-const newsFeed = 'newsFeed'
-const user = 'user'
-const dylink = 'dylink'
-
-export { cardetail, socketConnection, newsFeed, user, dylink }
